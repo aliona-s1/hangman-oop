d77185501448cc6915335634ce607155d628d546
Aliona Salavei <alionasalavei@gmail.com>
2024-11-06 00:14:32 +0400

    feat: add game start logic with user input

--- src/com/aliona/hangman/Main.java
+++ src/com/aliona/hangman/Main.java
@@ -1,7 +1,28 @@
 package com.aliona.hangman;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        startGame();
+    }
 
+    public static void startGame() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("[н]ачать новую игру или [в]ыйти из игры");
+
+        while (true) {
+            String letter = scanner.nextLine();
+
+            if (letter.equals("в")) {
+                System.out.println("Вы вышли из игры");
+                break;
+            } else if (letter.equals("н")) {
+                System.out.println("Вы начали игру");
+                break;
+            } else {
+                System.out.println("[н]ачать новую игру или [в]ыйти из игры");
+            }
+        }
     }
 }

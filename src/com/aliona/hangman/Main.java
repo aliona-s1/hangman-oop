23f24309af55d326dd320af856d4bfd3449f9cd8
Aliona Salavei <alionasalavei@gmail.com>
2024-11-06 23:00:12 +0400

    feat: show masked word

--- src/com/aliona/hangman/Main.java
+++ src/com/aliona/hangman/Main.java
@@ -9,20 +9,32 @@ public class Main {
 
     public static void startGame() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("[н]ачать новую игру или [в]ыйти из игры");
 
         while (true) {
+            System.out.println("[н]ачать новую игру или [в]ыйти из игры.");
             String letter = scanner.nextLine();
 
             if (letter.equals("в")) {
-                System.out.println("Вы вышли из игры");
+                System.out.println("Вы вышли из игры.");
                 break;
             } else if (letter.equals("н")) {
-                System.out.println("Вы начали игру");
+                System.out.println("Вы начали игру.");
+                showMaskedWord();
                 break;
             } else {
-                System.out.println("[н]ачать новую игру или [в]ыйти из игры");
+                System.out.println("Некорректный ввод.");
             }
         }
+    }
+
+    public static void showMaskedWord() {
+        String word = "телевизор";
+        String maskedWord = word.replaceAll(".", "_");
+
+        System.out.println("Текущее слово: " + maskedWord);
+    }
+
+    public static void revealGuessedLetters() {
+        // TODO: implement
     }
 }

820286ce404b62724cc39e872de799179aa115c1
Aliona Salavei <alionasalavei@gmail.com>
2024-11-21 23:56:44 +0400

    feat: add word loading from dictionary file

--- src/com/aliona/hangman/Main.java
+++ src/com/aliona/hangman/Main.java
@@ -1,10 +1,14 @@
 package com.aliona.hangman;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final Random random = new Random();
+    private static final List<String> words = new ArrayList<>();
     private static final String START = "да";
     private static final String EXIT = "нет";
     private static final String[] HANGMAN_STAGES =
@@ -87,11 +91,36 @@ public class Main {
     }
 
     public static void startGame() {
+        readDictionary();
+
+        if (words.isEmpty()) {
+            System.out.println("\nИзвините, ошибка!");
+            System.exit(1);
+        }
+
         System.out.println("\nВы начали игру.");
-        String word = "телевизор";
+
+        String word = words.get(random.nextInt(words.size()));
         String maskedWord = "_".repeat(word.length());
 
         startGameLoop(word, maskedWord);
+    }
+
+    private static void readDictionary() {
+        ClassLoader classLoader = Main.class.getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream("dictionary.txt")) {
+            try (Scanner scanner = new Scanner(inputStream)) {
+                while (scanner.hasNextLine()) {
+                    String word = scanner.nextLine().trim();
+                    if (!word.isEmpty()) {
+                        words.add(word);
+                    }
+                }
+            }
+        } catch (NullPointerException | IOException e) {
+            System.out.println("\nИзвините, ошибка!");
+            System.exit(1);
+        }
     }
 
     public static void startGameLoop(String word, String maskedWord) {
@@ -106,16 +135,12 @@ public class Main {
 
             if (!isInputLetterValid(letter)) {
                 System.out.println("\nВведите одну букву русского алфавита.");
-                continue;
-            }
-
-            if (isLetterAlreadyInput(letter, inputtedLetters)) {
+            } else if (isLetterAlreadyInput(letter, inputtedLetters)) {
                 System.out.println("\nЭта буква уже вводилась! Попробуйте другую.");
-                continue;
+            } else {
+                maskedWord = processGuessedLetter(word, letter, maskedWord, attemptRate, errorRate);
+                inputtedLetters.add(letter);
             }
-
-            maskedWord = processGuessedLetter(word, letter, maskedWord, attemptRate, errorRate);
-            inputtedLetters.add(letter);
 
             if (isWordGuessed(word, maskedWord)) {
                 System.out.println("\nПоздравляем! Вы угадали слово: " + word);
@@ -140,7 +165,7 @@ public class Main {
     }
 
     public static boolean isInputLetterValid(String letter) {
-        return letter.matches("[а-яё]");
+        return letter.matches("[а-яА-ЯЁё]");
     }
 
     public static boolean isLetterAlreadyInput(String letter, List<String> inputtedLetters) {
@@ -168,8 +193,8 @@ public class Main {
         for (int i = 0; i < word.length(); i++) {
             String currentLetter = word.substring(i, i + 1);
 
-            if (currentLetter.equals(letter)) {
-                updatedMaskedWord.replace(i, i + 1, letter);
+            if (currentLetter.equalsIgnoreCase(letter)) {
+                updatedMaskedWord.replace(i, i + 1, currentLetter);
             }
         }
         return updatedMaskedWord.toString();

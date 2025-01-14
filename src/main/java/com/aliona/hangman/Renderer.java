14f9a3eacbdb8a077791965d5a59180c10692932
Aliona Salavei <alionasalavei@gmail.com>
2025-01-14 15:24:22 +0400

    refactor: extract showStartMessage, showWinMessage and showLossMessage methods to Renderer class

--- src/main/java/com/aliona/hangman/Renderer.java
+++ src/main/java/com/aliona/hangman/Renderer.java
@@ -13,12 +13,24 @@ public class Renderer {
         System.out.print(hangmanStages[Math.min(mistakes, hangmanStages.length - 1)]);
     }
 
-    public void printMask(String mask) {
-        System.out.println(mask);
+    public void printMask(MaskedWord maskedWord) {
+        System.out.println(maskedWord.getMask());
+    }
+
+    public void showStartMessage() {
+        System.out.println("Начинаем игру!");
     }
 
     public void showGameInfo(int mistakes, int MAX_MISTAKES, List<String> inputtedLetters) {
         System.out.printf("Ошибок: %s из %s", mistakes, MAX_MISTAKES);
         System.out.println("\nВведенные буквы: " + String.join(",", inputtedLetters));
+    }
+
+    public void showWinMessage(MaskedWord maskedWord) {
+        System.out.printf("Поздравляем! Вы угадали слово: %s%n%n", maskedWord.getSecretWord());
+    }
+
+    public void showLossMessage(MaskedWord maskedWord) {
+        System.out.printf("Вы проиграли! Загаданное слово: %s%n%n", maskedWord.getSecretWord());
     }
 }

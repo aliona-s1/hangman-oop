6d196d3d3d62d7e83f39b6ce621f4bca5fc781e7
Aliona Salavei <alionasalavei@gmail.com>
2025-01-28 14:11:11 +0400

    refactor: move classMenu to Main

--- src/main/java/com/aliona/hangman/Main.java
+++ src/main/java/com/aliona/hangman/Main.java
@@ -3,13 +3,34 @@ package com.aliona.hangman;
 import java.util.Scanner;
 
 public class Main {
+    private static final String START = "Y";
+    private static final String EXIT = "N";
 
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary();
-        Scanner scanner = new Scanner(System.in);
         Renderer renderer = new Renderer();
+        Scanner scanner = new Scanner(System.in);
 
-        Menu menu = new Menu(scanner, renderer);
-        menu.startMenu(dictionary);
+        startMenu(dictionary, renderer, scanner);
+    }
+
+    private static void startMenu(Dictionary dictionary, Renderer renderer, Scanner scanner) {
+        System.out.printf("Хотите начать игру? %s/%s%n", START, EXIT);
+
+        while (true) {
+            String letter = scanner.nextLine().trim().toUpperCase();
+
+            if (letter.equals(START)) {
+                Game game = new Game(dictionary, renderer);
+                game.start(renderer);
+                System.out.printf("Хотите сыграть еще раз? %s/%s%n", START, EXIT);
+            } else if (letter.equals(EXIT)) {
+                System.out.println("Вы вышли из игры.");
+                scanner.close();
+                break;
+            } else {
+                System.out.printf("Некорректный ввод. Для начала игры - %s, для выхода - %s.%n", START, EXIT);
+            }
+        }
     }
 }
